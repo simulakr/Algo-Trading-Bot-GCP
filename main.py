f01ffe4067d54f21157c8db972da0e0175e9e048
Yunus Bilgiç <72708117+simulakr@users.noreply.github.com>
2025-09-05 13:48:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,13 +54,13 @@ class TradingBot:
             # Bybit zamanına göre next candle hesapla
             current_candle_ts = (ts // 900000) * 900000
             next_candle_ts = current_candle_ts + 900000
-            target_ts = next_candle_ts - 2000
+            target_ts = next_candle_ts - 1600
             wait_ms = target_ts - ts
             
             # next_candle_ts = (((ts // 900000) + 1) * 900000) - 2000  # 15 dakika = 900000 ms
             # wait_ms = next_candle_ts - ts + 800  # 1 saniye buffer
         
-            time.sleep(max(wait_ms / 1000, 1)) 
+            time.sleep(max(wait_ms / 1000, 2)) 
             logger.info("Yeni mum başladı - Veriler çekiliyor...")
         
         except Exception as e:
