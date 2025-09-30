eeb346f8b49c5437cbc96d55fd077a17231aa4ac
Yunus Bilgiç <72708117+simulakr@users.noreply.github.com>
2025-09-30 09:25:28 +0300

    Update entry_strategies.py

--- entry_strategies.py
+++ entry_strategies.py
@@ -1,7 +1,7 @@
 from typing import Dict, Any, Tuple
 
 def check_long_entry(row: Dict[str, Any]) -> bool:
-    return row['bb_3_touch_long_clean']==True
+    return row['atr_steps']=='long'
 
 def check_short_entry(row: Dict[str, Any]) -> bool:
-    return row['bb_3_touch_short_clean']==True
+    return row['atr_steps']=='short'
