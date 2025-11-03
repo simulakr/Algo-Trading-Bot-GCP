11e4e6dbfdca0d5daa978696e59bfb173af4e2b4
Yunus Bilgiç <72708117+simulakr@users.noreply.github.com>
2025-11-03 12:02:10 +0300

    Update entry_strategies.py

--- entry_strategies.py
+++ entry_strategies.py
@@ -1,7 +1,7 @@
 from typing import Dict, Any, Tuple
 
 def check_long_entry(row: Dict[str, Any]) -> bool:
-    return row['atr_steps']=='long'
+    return row['pivot_up_up']==True
 
 def check_short_entry(row: Dict[str, Any]) -> bool:
-    return row['atr_steps']=='short'
+    return row['pivot_down_down']==True
