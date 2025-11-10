2694ac49871e3781b25256f20eb7095ef6b1e557
Yunus Bilgiç <72708117+simulakr@users.noreply.github.com>
2025-11-10 13:12:20 +0300

    Update entry_strategies.py

--- entry_strategies.py
+++ entry_strategies.py
@@ -4,10 +4,10 @@ MAJOR_PAIRS_3X = ['BTCUSDT']
 
 def check_long_entry(row: Dict[str, Any], symbol: str) -> bool:
     atr_steps_col = 'pivot_go_up_3x' if symbol in MAJOR_PAIRS_3X else 'pivot_go_up_2x'
-    return row[atr_steps_col] == 'long'
+    return row[atr_steps_col] == True
 
 def check_short_entry(row: Dict[str, Any], symbol: str) -> bool:
     atr_steps_col = 'pivot_go_down_3x' if symbol in MAJOR_PAIRS_3X else 'pivot_go_down_2x'
-    return row[atr_steps_col] == 'short'
+    return row[atr_steps_col] == True
 
 
