6de8a4034da1d3849d07f9778ecc9d631d3d0468
Yunus Bilgiç <72708117+simulakr@users.noreply.github.com>
2025-11-26 16:06:13 +0300

    Update entry_strategies.py

--- entry_strategies.py
+++ entry_strategies.py
@@ -1,6 +1,6 @@
 from typing import Dict, Any, Tuple
 
-MAJOR_PAIRS_3X = ['BTCUSDT']
+MAJOR_PAIRS_3X = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT','XRPUSDT','DOGEUSDT']
 
 def check_long_entry(row: Dict[str, Any], symbol: str) -> bool:
     atr_steps_col = 'pivot_go_up_3x' if symbol in MAJOR_PAIRS_3X else 'pivot_go_up_2x'
