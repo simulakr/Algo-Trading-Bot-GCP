d0276d3ac602ca093893b2781f734aea7b5344eb
Yunus Bilgiç <72708117+simulakr@users.noreply.github.com>
2025-12-02 14:00:26 +0300

    Update entry_strategies.py

--- entry_strategies.py
+++ entry_strategies.py
@@ -1,13 +1,16 @@
 from typing import Dict, Any, Tuple
 
 MAJOR_PAIRS_3X = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT','XRPUSDT','DOGEUSDT']
+LONG_PAIRS_3X = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT','XRPUSDT']
+SHORT_PAIRS_3X = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT','XRPUSDT','DOGEUSDT']
 
 def check_long_entry(row: Dict[str, Any], symbol: str) -> bool:
-    atr_steps_col = 'pivot_go_up_3x' if symbol in MAJOR_PAIRS_3X else 'pivot_go_up_2x'
-    return row[atr_steps_col] == True
+    if symbol in LONG_PAIRS_3X:
+        return row['pivot_go_up_3x'] == True
+    return False
 
 def check_short_entry(row: Dict[str, Any], symbol: str) -> bool:
-    atr_steps_col = 'pivot_go_down_3x' if symbol in MAJOR_PAIRS_3X else 'pivot_go_down_2x'
+    atr_steps_col = 'pivot_go_down_3x' if symbol in SHORT_PAIRS_3X else 'pivot_go_down_2x'
     return row[atr_steps_col] == True
 
 
