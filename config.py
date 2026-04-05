aaa078d372edc2b9da2cf3befd3d07dc58ec14cf
Yunus Bilgiç <72708117+simulakr@users.noreply.github.com>
2026-04-05 23:25:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -61,16 +61,16 @@ TP_ROUND_NUMBERS = {
 }
 
 # Risk Yönetimi
-RISK_PER_TRADE_USDT = 40.0  # Per 20 USDT risk
+RISK_PER_TRADE_USDT = 20.0  # Per 20 USDT risk
 LEVERAGE = 25  # (max 25x)
 DEFAULT_LEVERAGE = 25
 
 SYMBOL_SETTINGS = {
-    'BTCUSDT': {'risk': 30.0, 'leverage': 25},
-    'ETHUSDT': {'risk': 30.0, 'leverage': 25},
-    'SOLUSDT': {'risk': 30.0, 'leverage': 25},
-    'XRPUSDT': {'risk': 30.0, 'leverage': 25},
-    'DOGEUSDT': {'risk': 30.0, 'leverage': 25}, # '1000PEPEUSDT': {'risk': 40.0, 'leverage': 20}
+    'BTCUSDT': {'risk': 20.0, 'leverage': 25},
+    'ETHUSDT': {'risk': 20.0, 'leverage': 25},
+    'SOLUSDT': {'risk': 20.0, 'leverage': 25},
+    'XRPUSDT': {'risk': 20.0, 'leverage': 25},
+    'DOGEUSDT': {'risk': 20.0, 'leverage': 25}, # '1000PEPEUSDT': {'risk': 40.0, 'leverage': 20}
 }
 
 # Trading Mode
