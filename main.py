8573c97eddae67691363dad30c3661b78d6692aa
Yunus Bilgiç <72708117+simulakr@users.noreply.github.com>
2026-04-27 09:20:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -231,10 +231,10 @@ class TradingBot:
 
             for target in [15, 30, 45, 0]:
                 if target == 0:
-                    target_time = current.replace(minute=0, second=0, microsecond=700000) + datetime.timedelta(hours=1)
+                    target_time = current.replace(minute=0, second=0, microsecond=400000) + datetime.timedelta(hours=1)
                     break
                 if minute < target:
-                    target_time = current.replace(minute=target, second=0, microsecond=700000)
+                    target_time = current.replace(minute=target, second=0, microsecond=400000)
                     break
 
             if target_time <= current:
